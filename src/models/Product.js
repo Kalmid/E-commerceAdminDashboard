a929218034fe0a2b425c765c39b0219debd8ca28
Kalmi Dilara <kalmid.98@gmail.com>
2026-04-16 20:25:35 +0530

    Create Product model with fields

---
+++ src/models/Product.js
@@ -0,0 +1,25 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/database");
+
+const Product = sequelize.define("Product", {
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.TEXT,
+  },
+  price: {
+    type: DataTypes.FLOAT,
+    allowNull: false,
+  },
+  stock: {
+    type: DataTypes.INTEGER,
+    defaultValue: 0,
+  },
+  imageUrl: {
+    type: DataTypes.STRING,
+  },
+});
+
+module.exports = Product;
