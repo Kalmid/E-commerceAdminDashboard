31839b5cbd8bc824c09abcb83ac09b54dff0e833
Kalmi Dilara <kalmid.98@gmail.com>
2026-04-16 15:09:08 +0530

    Set up database connection successfully

--- src/app.js
+++ src/app.js
@@ -1,13 +1,18 @@
 const express = require("express");
+const sequelize = require("./config/database");
 
 const app = express();
 
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("API is running 🚀");
+  res.send("API is running");
 });
 
+//Test DB connection
+sequelize.authenticate()
+  .then(() => console.log("Database connected successfully"))
+  .catch((err) => console.error("DB Error:", err));
 
 app.listen(3000, () => {
   console.log("Server running on http://localhost:3000");
